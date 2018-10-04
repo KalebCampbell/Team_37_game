64b990de495049db75b7292177a5c7527c058d77
Michael <michaelvincentnz@gmail.com>
2018-10-04 14:40:28 +1300

    Added Main class
    Added Main class so GUI doesn't need one.

--- AbandonedPrison/src/Main/Main.java
+++ AbandonedPrison/src/Main/Main.java
@@ -1,15 +1,53 @@
 package Main;
 
+import Application.GUI;
+import GameWorld.Game;
+
 public class Main {
 	
 	public static void main(String[] args) {
 	// ===========================================
 	// ===== Parse in command-line arguments =====
 	// ===========================================
+	String fileName = null;
+	int timeLimit = 0;
+	
+	// Loop over arguments
+	// TODO: Need some error detection for invalid arguments
+	for(int i = 0; i != args.length; i++) {
+		if(args[i].startsWith("-")) {
+			String arg = args[i];
+			if(arg.equals("-help")) {
+				// Go into a usage menu.
+			}
+		}else {
+			fileName = args[0];
+			timeLimit = Integer.parseInt(args[1]);
+				// Should be filename(map), 1,2,3,4,5 for time limits in minutes
+		}
+	}
+	//startGame(fileName,timeLimit);
+	startGame();
+	}
+	
+	public static void startGame(){
+		GUI gui = new GUI();
+		//game.begin();
+		
+		// Main game loop here?
+		
+	}
+	
+	
+	
 	
 	// Map argument
+	
+	
 	// Time-limit argument (Complete the game in set time)
 	// Cheats on/off
+		
+	
 		
 		
 	// Main game loop
@@ -18,5 +56,3 @@ public class Main {
 	
 	}
 	
-
-}
