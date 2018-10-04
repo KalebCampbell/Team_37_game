c05e7a7e93e6f3667d2bac0731274b4cc8cc9328
kaleb <kaleb1997@xtra.co.nz>
2018-10-04 23:06:29 +1300

    added Room component and updated parser

--- AbandonedPrison/src/Persistence/Dom_Parser.java
+++ AbandonedPrison/src/Persistence/Dom_Parser.java
@@ -3,15 +3,9 @@ package Persistence;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-
-import GameWorld.Player;
-import GameWorld.Room;
-import GameWorld.Wall;
 
 public class Dom_Parser {
 
@@ -20,41 +14,29 @@ public class Dom_Parser {
 	public static void main(String[] args) {
 	
 		
-		//North = 0 
-		//East = 1
-		//West = 2
-		//South = 3
+		//creates a room and sets all the details for it to be created into XML format.
+		RoomComponent room  = new RoomComponent();
+		room.setId(0);
+		room.setLocX(0);
+		room.setLocY(0);
+		room.getHasPlayer();
 		
-		
-		//Character player = new Character();
-		//player.addLocation(0, 0);
-		 
-			
-		Room room  = new Room();
-		room.setRoomID(0);
-		//room.setLocation(0,0);
-		//room.hasPlayer(true);
-		
+		//sets the walls for the wall arraylist to be added to the specific room.
 		List<Integer> walls = new ArrayList<>();
 		walls.add(1);
 		walls.add(2);
 		walls.add(3);
 		
-		//the item needs to have the name of the item and the location. 
-		//List<Item> items = new ArrayList<>();
-		//items.add(Key, 0, 0);
-		
-		
-		//sets the walls of the room to an array of [0, 1, 2, 3,] depending on the walls. 
+		//adds the walls to the room
 		room.setWalls(walls);
 		
-		//room.setItems(items);
 		xmlRooms(room);
+		
 	}
-	public static void xmlRooms(Room room) {
+	public static void xmlRooms(RoomComponent room) {
 		try {
 			File file = new File("file.xml");
-			JAXBContext jaxbContext = JAXBContext.newInstance(Room.class);
+			JAXBContext jaxbContext = JAXBContext.newInstance(RoomComponent.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 			
 			//output printed pretty
